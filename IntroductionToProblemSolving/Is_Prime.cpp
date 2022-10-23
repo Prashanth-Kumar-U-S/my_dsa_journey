652bbfd4c6612c5a0a0cfc9eba1a4efbf3daa176
Prashanth Kumar U S <81318710+prashanth-us@users.noreply.github.com>
2022-10-23 17:47:19 +0530

    Add is it prime

---
+++ IntroductionToProblemSolving/Is_Prime.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()  {
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'scanf' for input & 'printf' for output
+    int A, i, flag;
+    scanf("%d", &A);
+    
+    flag = 1;
+    
+    for (i = 2; i < A; i++){
+        if(A % i == 0){
+            // i divides A , therefore it is not a prime
+            flag = 0;
+            break;
+        }
+    }
+    
+    if(flag){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+    return 0;
+}
